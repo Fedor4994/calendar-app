bbb739dd4043cc26393e5a097bd395479addc41b
Fedor4994 <fedorsosnin04@gmail.com>
2024-09-28 14:52:44 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  ScheduleComponent,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  Inject,
+  Resize,
+  DragAndDrop
+} from "@syncfusion/ej2-react-schedule";
+import { scheduleData } from "./events";
+import { registerLicense } from "@syncfusion/ej2-base";
 
-function App() {
+
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NDaF5cWWtCf1NpR2RGfV5ycEVBal1XTnVYUj0eQnxTdEFjUH1WcHNXRGRVU0xzXQ==')
+
+
+const Calendar = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <ScheduleComponent
+        height="650px"
+        eventSettings={{ dataSource: scheduleData }}
+        selectedDate={new Date(2024, 9, 1)}
+      >
+        <Inject
+          services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+        />
+      </ScheduleComponent>
     </div>
   );
-}
+};
 
-export default App;
+export default Calendar;
